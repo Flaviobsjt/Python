355b0678f7c29bafb62af8b28a96d6cd14ee1e30
Flaviobsjt <flaviobsjt@gmail.com>
2023-10-16 19:15:39 -0300

    Organizando EX012

--- Exercícios Curso em Vídeo/EX012.py
+++ Exercícios Curso em Vídeo/EX012.py
@@ -1,4 +1,4 @@
-# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto
+# Exercício da aula: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto
 
 preço=float(input('Qual o preço do seu produto? R$'))
 novo= preço - (preço * 5 / 100)
